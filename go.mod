338b99b3ead868ebf8363bf46ef72ce9cb0c367b
Ubuntu <ubuntu@ip-172-31-39-16.ec2.internal>
2020-05-17 13:36:15 +0000

    TenantActor: Done VerifierActore: Half

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module housingSystem
 go 1.14
 
 require (
+	github.com/AsynkronIT/goconsole v0.0.0-20160504192649-bfa12eebf716
+	github.com/AsynkronIT/protoactor-go v0.0.0-20200317173033-c483abfa40e2
 	github.com/astaxie/beego v1.12.1
 	github.com/go-sql-driver/mysql v1.4.1
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.4.2
 	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+	google.golang.org/protobuf v1.23.0
 )
---
+++ src/tenant/tenant.go
@@ -0,0 +1,61 @@
+package tenant
+
+import (
+	// "runtime"
+	"fmt"
+	// console "github.com/AsynkronIT/goconsole"
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/remote"
+	tenantMessage "housingSystem/src/messages/tenant"
+	"housingSystem/src/shared"
+	// _ "housingSystem/src/verifier"
+)
+
+// Actor The Tenant Actor
+type Actor struct {
+	shared.BaseActor
+	verifier *actor.PID
+	distributor *actor.PID
+}
+
+// TriggerNewApplicationRequest A message that will trigger a new application
+type TriggerNewApplicationRequest struct {
+}
+
+// TriggerNewApplicationResponse A message that will response trigger a new application
+type TriggerNewApplicationResponse struct {
+	Message string
+}
+
+// Receive Receive function for Tenant Actor
+func (t *Actor) Receive(ctx actor.Context) {
+	switch ctx.Message().(type) {
+	case *actor.Started:
+		fmt.Println("Tenant Starting, initialize actor here, PID:", ctx.Self())
+		t.Self = ctx.Self()
+		t.verifier = actor.NewPID("127.0.0.1:8080", "Verifier")
+		// t.distributor = actor.NewPID("127.0.0.1:8081", "Distributor")
+		// shared.Use(verifierActor)
+
+	case *actor.Stopped:
+		fmt.Println("Tenant Stopped, actor and its children are stopped")
+	case *TriggerNewApplicationRequest:
+		fmt.Println("Tenant Ready to request.")
+		ctx.Send(t.verifier, &tenantMessage.HouseApplicationRequest{
+			UserId: 1,
+			UserName: "Chaokun",
+		})
+
+	case *tenantMessage.HouseApplicationResponse:
+		fmt.Println("The application submitted to ", ctx.Sender())
+	}
+}
+
+func newTenantActor() actor.Actor {
+	return &Actor{}
+}
+
+func init() {
+	remote.Start("127.0.0.1:9001")
+	remote.Register("Tenant", actor.PropsFromProducer(newTenantActor))
+}
