f6ace5bf0303b3584da5218c14c3459e09dfe7b5
Ubuntu <ubuntu@ip-10-20-9-85.ec2.internal>
2020-06-07 00:44:46 +0000

    get protoc-gen-go in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 go get google.golang.org/grpc
 go get github.com/go-sql-driver/mysql
 go get github.com/golang/protobuf
+go get github.com/golang/protobuf/protoc-gen-go
 go get github.com/AsynkronIT/goconsole
 go get github.com/AsynkronIT/protoactor-go/actor
 
