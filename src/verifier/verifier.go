cad9a5ad69c1603a2c94f6e7c38f409e4ec0411f
EZ-hwh <240836592@qq.com>
2021-01-22 01:55:07 +0800

    add checkout logic

--- src/verifier/verifier.go
+++ src/verifier/verifier.go
@@ -1,8 +1,11 @@
 package verifier
 
 import (
+	"Reactive-Welfare-Housing-System/src/config"
+	"Reactive-Welfare-Housing-System/src/utils"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	// verifierMessage "Reactive-Welfare-Housing-System/src/messages/verifier"
@@ -15,17 +18,42 @@ import (
 	"github.com/AsynkronIT/protoactor-go/actor"
 )
 
+type checkoutLog struct {
+	log 			[]*verifierMessages.HouseCheckOut
+	commitIndex		int
+	Mu 				sync.RWMutex
+}
 // Actor The  Verifier Actor
 type verifierActor struct {
 	shared.BaseActor
-	mamagerPID     *actor.PID
-	distributorPID *actor.PID
-	governmentPID  *actor.PID
+	mamagerPID     	*actor.PID
+	distributorPID 	*actor.PID
+	governmentPID  	*actor.PID
+	newcheckoutLog	checkoutLog
 }
 
 // Receive Receive function for Verifier Actor
 func (v *verifierActor) Receive(ctx actor.Context) {
 	switch msg := ctx.Message().(type) {
+	case *actor.Started:
+		go func() {
+			select {
+			//300ms
+				case <- time.Tick(config.TIME_OUT * time.Millisecond):
+					go func() {
+						v.newcheckoutLog.Mu.RLock()
+						checkoutCommitIndex := v.newcheckoutLog.commitIndex
+						checkouts := v.newcheckoutLog.log[checkoutCommitIndex:]
+						v.newcheckoutLog.Mu.RUnlock()
+						if len(checkouts) > 0 {
+							ctx.Request(v.distributorPID, &verifierMessages.HouseCheckOuts{
+								Checkouts: checkouts,
+								CommitIndex: int32(checkoutCommitIndex)
+							})
+						}
+					}()
+			}
+		}()
 	// case *actor.Started:
 	// 	fmt.Println("Verifier Starting, initialize actor here, PID:", ctx.Self())
 	// t.distributor = actor.NewPID("127.0.0.1:8081", "Distributor")
@@ -74,9 +102,37 @@ func (v *verifierActor) Receive(ctx actor.Context) {
 			}
 		})
 	case *sharedMessages.NewCheckOuts:
+		v.newcheckoutLog.Mu.Lock()
+		pstart := len(v.newcheckoutLog.log)
+		//v.newcheckoutLog.log = append(v.newcheckoutLog.log, msg.CheckOuts...)
+		var checkouts []verifierMessages.HouseCheckOut
 		for _, checkout := range msg.CheckOuts {
-			ctx.Self().Tell(checkout)
+			v.newcheckoutLog.log = append(v.newcheckoutLog.log, verifierMessages.HouseCheckOut{
+				FamilyID: 	checkout.FamilyID,
+				Level: 	  	checkout.Level,
+			})
+			checkouts = append(checkouts, verifierMessages.HouseCheckOut{
+				UserInfo: nil,
+				FamilyID: checkout.FamilyID,
+				Level:    checkout.Level,
+			})
 		}
+		v.newcheckoutLog.Mu.Unlock()
+
+		pstart0 := pstart
+		v.newcheckoutLog.Mu.RLock()
+		commitIndex := v.newcheckoutLog.commitIndex
+		v.newcheckoutLog.Mu.RUnlock()
+		ctx.Request(v.distributorPID, &verifierMessages.HouseCheckOuts{
+			Checkouts:   v.newcheckoutLog.log[commitIndex : pstart0 + len(checkouts)],
+			CommitIndex: int32(commitIndex),
+		})
+
+	case *distributorMessages.HouseCheckOutACK:
+		v.newcheckoutLog.Mu.Lock()
+		v.newcheckoutLog.commitIndex = utils.Max(v.newcheckoutLog.commitIndex, int(msg.CommitIndex))
+		v.newcheckoutLog.Mu.Unlock()
+
 	case *managerMessages.HouseMatchApprove:
 		log.Print("Verifier: Received HouseMatchApprove, ", msg.Match)
 		ctx.Respond(&verifierMessages.HouseMatchApproveACK{})
